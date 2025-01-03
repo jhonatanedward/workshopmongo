9962a4f5810dbc0fe6a7022b9e35930ce415b051
Jhonatan Edward <jhonatanedsp@gmail.com>
2025-01-03 11:34:48 -0300

    Método post users

--- src/main/java/com/jedward/workshopmongo/resources/UserResource.java
+++ src/main/java/com/jedward/workshopmongo/resources/UserResource.java
@@ -1,14 +1,15 @@
 package com.jedward.workshopmongo.resources;
 
 import com.jedward.workshopmongo.domain.User;
+import com.jedward.workshopmongo.resources.dto.UserDTO;
 import com.jedward.workshopmongo.services.UserService;
 import lombok.AllArgsConstructor;
+import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -27,6 +28,15 @@ public class UserResource {
     @GetMapping("{id}")
     public ResponseEntity<User> buscaPorId(@PathVariable String id) {
         return ResponseEntity.ok().body(userService.findUserById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> insert(@RequestBody UserDTO userDTO) {
+        User user = userService.fromDTO(userDTO);
+        user = userService.insert(user);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(user.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 
 }
--- src/main/java/com/jedward/workshopmongo/services/UserService.java
+++ src/main/java/com/jedward/workshopmongo/services/UserService.java
@@ -2,6 +2,7 @@ package com.jedward.workshopmongo.services;
 
 import com.jedward.workshopmongo.domain.User;
 import com.jedward.workshopmongo.repositories.UserRepository;
+import com.jedward.workshopmongo.resources.dto.UserDTO;
 import com.jedward.workshopmongo.services.exception.ObjectNotFoundException;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -21,5 +22,13 @@ public class UserService {
     public User findUserById(String id) {
         Optional<User> retorno = userRepository.findById(id);
         return retorno.orElseThrow(() -> new ObjectNotFoundException("Usuário não encontrado"));
+    }
+
+    public User insert(User user) {
+        return userRepository.insert(user);
+    }
+
+    public User fromDTO(UserDTO userDto) {
+        return new User(userDto.getId(), userDto.getName(), userDto.getEmail());
     }
 }
